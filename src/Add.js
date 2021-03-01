ef0e327b406d96f11b4de506181b54097951699d
yechu <yechukim217@gmail.com>
2021-03-02 00:11:17 +0900

    fetch json separated

---
+++ src/Add.js
@@ -0,0 +1,11 @@
+const Add = () => {
+  return ( 
+    <div className="add">
+      <h2>Add something </h2>
+
+    </div>
+
+   );
+}
+ 
+export default Add;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <div className="navbar">
       <h2 className="title">Fun Cat Facts</h2>
       <div className="nav-lists">
+        <Link to="/">Home</Link>
+        <a href="https://alexwohlbruck.github.io/cat-facts/docs/">API</a>
+        <Link to="/add">Add </Link>
+      </div>
 
-      <a href="https://alexwohlbruck.github.io/cat-facts/docs/">API</a>
-      <a>Add </a>
-      </div>
-    
-      </div>
+    </div>
   );
 }
 
